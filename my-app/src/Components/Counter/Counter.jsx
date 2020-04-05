6d89991f36be6de539449d1cdafde4c9fd1a03fa
Acrux666444 <dawgoc66@gmail.com>
2020-04-05 18:57:23 +0200

    Front-1 end exercise

--- my-app/src/Components/Counter/Counter.jsx
+++ my-app/src/Components/Counter/Counter.jsx
@@ -10,31 +10,72 @@ class Counter extends Component{
             counter : 0
         }
         this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
     }
 
-    render(){
-      return (
-        <div className = "counter">
-            <button onClick={this.increment}>{this.props.by}</button>
-            <span className = "count">{this.state.counter}</span>
-        </div>
-      );
-    }
-
-    increment(){
+    render() {
+        return (
+          <div className = "counter">
+            <CounterButton by = {100} incrementMethod = {this.increment} decrementMethod = {this.decrement}/>
+            <CounterButton by = {10} incrementMethod = {this.increment} decrementMethod = {this.decrement}/>
+            <CounterButton by = {2} incrementMethod = {this.increment} decrementMethod = {this.decrement}/>
+            <CounterButton by = {1} incrementMethod = {this.increment} decrementMethod = {this.decrement}/>
+            <div className = "countBlock"> 
+                <span className = "count">{this.state.counter}</span>
+            </div>
+            <button className = "reset" onClick = {this.reset}>Reset</button>
+          </div>
+          );
+      }
+      reset(){
         this.setState({
-            counter: this.state.counter + this.props.by
+            counter: 0
         });
+        console.log('reset');
+      }
+
+      increment(by) {
+        this.setState(
+            (prevState) => {
+               return  {counter: prevState.counter + by}
+            }
+        );
+        console.log('inceremnt');
+    }
+
+    decrement(by) {
+        this.setState(
+            (prevState) => {
+               return  {counter: prevState.counter - by}
+            }
+        );
         console.log('inceremnt');
     }
 
 }
-Counter.defaultProps = {
+
+class CounterButton extends Component{
+
+    render(){
+      return (
+        <div className = "counterButton">
+            <button onClick={() => this.props.incrementMethod(this.props.by)}>{'+' + this.props.by}</button>
+            <button onClick={() => this.props.decrementMethod(this.props.by)}>{'-' + this.props.by}</button>
+        </div>
+      );
+    }
+}
+
+
+CounterButton.defaultProps = {
     by: 1
 }
-Counter.propTypes = {
+CounterButton.propTypes = {
     by: PropTypes.number
 }
+
+
 
   
 
